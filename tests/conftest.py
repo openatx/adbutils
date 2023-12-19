23204d3b483da2692dc783ebe489911942d12b15
Cleo Charollais <6610554-clchrlls@users.noreply.gitlab.com>
2023-12-19 16:36:13 +0100

    added pulldir tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@
 
 import pytest
 from adbutils import adb, AdbDevice
+import subprocess
+import pathlib
 
 
 @pytest.fixture(scope="session")
@@ -15,3 +17,26 @@ def device_tmp_path(device: AdbDevice):
     tmp_path = "/data/local/tmp/Hi-世界.txt"
     yield tmp_path
     device.remove(tmp_path)
+
+@pytest.fixture
+def device_tmp_dir_path(device: AdbDevice):
+    tmp_dir_path = "/sdcard/test_d"
+    yield tmp_dir_path
+    device.remove_dir(tmp_dir_path)
+
+@pytest.fixture
+def local_src_in_dir():
+    local_src_in_dir = pathlib.Path('tests/test-assets/test_s')
+    yield local_src_in_dir
+
+@pytest.fixture
+def local_src_out_dir1():
+    local_src_out_dir1 = pathlib.Path('tests/test-assets/test_d1')
+    yield local_src_out_dir1
+    subprocess.check_output(['rm', '-r', local_src_out_dir1])
+
+@pytest.fixture
+def local_src_out_dir2():
+    local_src_out_dir2 = pathlib.Path('tests/test-assets/test_d2')
+    yield local_src_out_dir2
+    subprocess.check_output(['rm', '-r', local_src_out_dir2])
--- tests/test_device.py
+++ tests/test_device.py
@@ -4,10 +4,12 @@ extra functions test
 效果生效与否不好判定（例如屏幕亮暗），部分用例仅作冒烟测试
 """
 
+import os
 import io
 import pathlib
 import re
 import time
+import filecmp
 
 import pytest
 
@@ -185,3 +187,66 @@ def test_logcat(device: AdbDevice, tmp_path: pathlib.Path):
     logcat.stop()
     assert logcat_path.exists()
     assert re.compile(r"I/TAG.*hello").search(logcat_path.read_text(encoding="utf-8"))
+
+
+# todo: make independent of already present stuff on the phone
+def test_pull_push_dirs(
+        device: AdbDevice, 
+        device_tmp_dir_path:str, 
+        local_src_in_dir:pathlib.Path, 
+        local_src_out_dir1:pathlib.Path, 
+        local_src_out_dir2:pathlib.Path
+    ):
+
+    def are_dir_trees_equal(dir1, dir2):
+        """
+        Compare two directories recursively. Files in each directory are
+        assumed to be equal if their names and contents are equal.
+
+        NB: retreived from: https://stackoverflow.com/a/6681395
+
+        @param dir1: First directory path
+        @param dir2: Second directory path
+
+        @return: True if the directory trees are the same and 
+            there were no errors while accessing the directories or files, 
+            False otherwise.
+        """
+
+        dirs_cmp = filecmp.dircmp(dir1, dir2)
+        if len(dirs_cmp.left_only)>0 or len(dirs_cmp.right_only)>0 or \
+            len(dirs_cmp.funny_files)>0:
+            return False
+        (_, mismatch, errors) =  filecmp.cmpfiles(
+            dir1, dir2, dirs_cmp.common_files, shallow=False)
+        if len(mismatch)>0 or len(errors)>0:
+            return False
+        for common_dir in dirs_cmp.common_dirs:
+            new_dir1 = os.path.join(dir1, common_dir)
+            new_dir2 = os.path.join(dir2, common_dir)
+            if not are_dir_trees_equal(new_dir1, new_dir2):
+                return False
+        return True
+
+
+    device.push(local_src_in_dir.absolute().__str__(), device_tmp_dir_path)
+
+    device.sync.pull_dir(device_tmp_dir_path, local_src_out_dir1)
+
+    assert local_src_out_dir1.exists()
+    assert local_src_out_dir1.is_dir()
+
+    are_dir_trees_equal(local_src_in_dir, local_src_out_dir1)
+    
+
+    device.sync.pull(device_tmp_dir_path, local_src_out_dir2)
+
+    assert local_src_out_dir2.exists()
+    assert local_src_out_dir2.is_dir()
+
+    are_dir_trees_equal(local_src_in_dir, local_src_out_dir2)
+
+
+
+
+
