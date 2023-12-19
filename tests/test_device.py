d73e6a76933808c62dd2b0241fcd4af56a22b915
Cleo Charollais <6610554-clchrlls@users.noreply.gitlab.com>
2023-12-19 16:52:47 +0100

    added additional pull_file test

--- tests/test_device.py
+++ tests/test_device.py
@@ -125,6 +125,29 @@ def test_sync_pull_push(device: AdbDevice, device_tmp_path, tmp_path: pathlib.Pa
         data += chunk
     assert b"Hello Android" == data
 
+def test_sync_pull_file_push(device: AdbDevice, device_tmp_path, tmp_path: pathlib.Path):
+    src = io.BytesIO(b"Hello 1")
+    device.sync.push(src, device_tmp_path)
+    assert b"Hello 1" == device.sync.read_bytes(device_tmp_path)
+
+    device.sync.push(b"Hello 12", device_tmp_path)
+    assert "Hello 12" == device.sync.read_text(device_tmp_path)
+
+    target_path = tmp_path / "hi.txt"
+    target_path.write_text("Hello Android")
+    dst_path = tmp_path / "dst.txt"
+    dst_path.unlink(missing_ok=True)
+    
+    device.sync.push(target_path, device_tmp_path)
+    assert "Hello Android" == device.sync.read_text(device_tmp_path)
+    device.sync.pull_file(device_tmp_path, dst_path)
+    assert "Hello Android" == dst_path.read_text(encoding="utf-8")
+
+    data = b""
+    for chunk in device.sync.iter_content(device_tmp_path):
+        data += chunk
+    assert b"Hello Android" == data
+
 
 def test_screenshot(device: AdbDevice):
     im = device.screenshot()
