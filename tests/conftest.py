08fcef803d47917ed7cede00918d7953e7aec159
Cleo Charollais <6610554-clchrlls@users.noreply.gitlab.com>
2023-12-29 11:06:17 +0100

    changed test to use tmpdir and tmp_path fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,18 +25,44 @@ def device_tmp_dir_path(device: AdbDevice):
     device.remove_dir(tmp_dir_path)
 
 @pytest.fixture
-def local_src_in_dir():
-    local_src_in_dir = pathlib.Path('tests/test-assets/test_s')
-    yield local_src_in_dir
+def local_src_in_dir(tmpdir):
 
-@pytest.fixture
-def local_src_out_dir1():
-    local_src_out_dir1 = pathlib.Path('tests/test-assets/test_d1')
-    yield local_src_out_dir1
-    subprocess.check_output(['rm', '-r', local_src_out_dir1])
+    tmpdir.join('1.txt').write('1\n')
+    tmpdir.join('2.txt').write('2\n')
+    tmpdir.join('3.txt').write('3\n')
 
-@pytest.fixture
-def local_src_out_dir2():
-    local_src_out_dir2 = pathlib.Path('tests/test-assets/test_d2')
-    yield local_src_out_dir2
-    subprocess.check_output(['rm', '-r', local_src_out_dir2])
+    a = tmpdir.mkdir('a')
+    a.join('a1.txt').write('a1\n')
+
+    aa = a.mkdir('aa')
+    aa.join('aa1.txt').write('aa1\n')
+
+    ab = a.mkdir('ab')
+    ab.join('ab1.txt').write('ab1\n')
+    ab.join('ab2.txt').write('ab2\n')
+
+    b = tmpdir.mkdir('b')
+    b.join('b1.txt').write('b1\n')
+
+    c = tmpdir.mkdir('c')
+    ca = c.mkdir('ca')
+    ca.join('ca1.txt').write('ca1\n')
+
+    caa = ca.mkdir('caa')
+    caa.join('caa1.txt').write('caa1\n')
+
+    cb = c.mkdir('cb')
+
+    yield tmpdir
+
+# @pytest.fixture
+# def local_src_out_dir1(tmpdir):
+#     local_src_out_dir1 = pathlib.Path('tests/test-assets/test_d1')
+#     yield local_src_out_dir1
+#     subprocess.check_output(['rm', '-r', local_src_out_dir1])
+
+# @pytest.fixture
+# def local_src_out_dir2(tmpdir):
+#     local_src_out_dir2 = pathlib.Path('tests/test-assets/test_d2')
+#     yield local_src_out_dir2
+#     subprocess.check_output(['rm', '-r', local_src_out_dir2])
